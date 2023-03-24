6a723345dbb02ac36c850488d6a055b89d41b416
hotforestfruit <82053710+hotforesftruit@users.noreply.github.com>
2023-03-24 11:27:12 -0500

    Add functionality for multiple operations

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+
+//Arrays for storing 2 numbers to be evaluated and operator
 let num1=[];
 let num2=[];
 let operator;
+//Variable for display area
 let displayArea = document.querySelector('#display');
+//Array for inputs
 let inputValue=[];
-let displayValue=[];
 let opIn = false;
+let solved = false;
 function add(a,b){
     return a+b;
 }
@@ -32,13 +36,15 @@ const inputNumbers = document.querySelectorAll("#numbers > button");
 inputNumbers.forEach((button) =>{
     button.addEventListener('click',()=>{
         
-        displayValue.push(button.id);
-        displayArea.innerHTML = `<h3>${displayValue.join('')}</h3>`;
-        if(!opIn){
+        inputValue.push(button.id);
+        //displayArea.innerHTML = `<h3>${inputValue.join('')}</h3>`;
+        if(!opIn && !solved){
             num1.push(button.id);
+            displayArea.innerHTML= `<h3>${num1.join('')}</h3>`;
         }else{
             num2.push(button.id);
-        }
+            displayArea.innerHTML= `<h3>${num2.join('')}</h3>`;
+        } 
     })
     
 })
@@ -46,35 +52,52 @@ inputNumbers.forEach((button) =>{
 const inputOps = document.querySelectorAll("#operators > button");
 inputOps.forEach((button) =>{
     button.addEventListener('click',()=>{
-        displayValue.push(button.id);
-        displayArea.innerHTML = `<h3>${displayValue.join('')}</h3>`;
+        inputValue.push(button.id);
+        /* displayArea.innerHTML = `<h3>${inputValue.join('')}</h3>`; */
         if(opIn){
             opIn=false;
         }else if(!opIn){
             opIn=true;
         }
         operator=button.id;
+        if(!solved){
+            solution = operate(
+                parseInt(num1.join('')),
+                operator,parseInt(num2.join('')));
+            solved=true;
+            
+        }else{
+            solution = operate(
+                parseInt(num1.join('')),
+                operator,parseInt(num2.join('')));
+            solved=false;
+            /* displayArea.innerHTML = `<h3>${solution}</h3>` */
+        }
     })
     
 })
 const clear = document.querySelector("#clear");
 clear.addEventListener('click', ()=> {
-    displayValue = [];
-    displayArea.innerHTML = `<h3>${displayValue}</h3>`
+    inputValue = [];
+    displayArea.innerHTML = `<h3>${inputValue}</h3>`
     opIn=false;
     operator=[];
-    inputValue=[];
-    displayValue=[];
+    num1=[];
+    num2=[];
+    solved=false;
 })
 const solve = document.querySelector('#equals');
 solve.addEventListener('click', ()=> {
     
-    solution = operate(parseInt(num1.join('')),operator,parseInt(num2.join('')));
-    console.log(solution);
+    solution = operate(
+        parseInt(num1.join('')),
+        operator,parseInt(num2.join('')));
+    //console.log(solution);
+    console.log(inputValue)
     displayArea.innerHTML = `<h3>${solution}</h3>`
-    inputValue = [];
     opIn=false;
-    operator=0;
-    num1=[];
+    operator=[];
+    num1 = [solution];
     num2=[];
+    solved=true;
 })
